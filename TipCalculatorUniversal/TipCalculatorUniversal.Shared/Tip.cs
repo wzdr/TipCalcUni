a19515f43fb2d1ecef69f637d686446acea9acbd
Wolfgang Zdrenka <wzdrenka@gmail.com>
2014-11-02 22:34:44 +1300

    Chapter 10 Tip calculator as universal app

---
+++ TipCalculatorUniversal/TipCalculatorUniversal.Shared/Tip.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalculatorUniversal
+{
+    public class Tip
+    {
+        public string BillAmount { get; set; }
+        public string TipAmount { get; set; }
+        public string TotalAmount { get; set; }
+
+        public Tip()
+        {
+            this.BillAmount = String.Empty;
+            this.TipAmount = String.Empty;
+            this.TotalAmount = String.Empty;
+        }
+
+        public void CalculateTip(string origAmount, double tipPercentage)
+        {
+            double billAmount = 0.0;
+            double tipAmount = 0.0;
+            double totalAmount = 0.0;
+
+            if (double.TryParse(origAmount.Replace('$', ' '), out billAmount))
+            {
+                tipAmount = billAmount * tipPercentage;
+                totalAmount = billAmount + tipAmount;
+            }
+
+            this.BillAmount = String.Format("{0:C}", billAmount);
+            this.TipAmount = String.Format("{0:C}", tipAmount);
+            this.TotalAmount = String.Format("{0:C}", totalAmount);
+        }
+    }
+}
---
+++ TipCalculatorUniversal/TipCalculatorUniversal.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TipCalculatorUniversal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Tip tip;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            tip = new Tip();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void amountTextBlock_LostFocus(object sender, RoutedEventArgs e)
+        {
+            billAmountTextBox.Text = tip.BillAmount;
+        }
+
+        private void billAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void amountTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            billAmountTextBox.Text = "";
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void performCalculation()
+        {
+            var selectedRadio = myStackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+
+            tip.CalculateTip(billAmountTextBox.Text, double.Parse(selectedRadio.Tag.ToString()));
+
+            amountToTipTextBlock.Text = tip.TipAmount;
+            totalTextBlock.Text = tip.TotalAmount;
+        }
+    }
+}
